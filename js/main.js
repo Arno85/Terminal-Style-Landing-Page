ac8022ca10a38b59ef226fc88b5ec2d7010d92dc
Arnaud <arnaud.martin85@gmail.com>
2021-05-12 10:36:47 -0400

    terminal style landing page

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const about = document.querySelector('#about');
+const aboutContent = document.querySelector('#about-content');
+const contact = document.querySelector('#contact');
+const contactContent = document.querySelector('#contact-content');
+
+const createBox = (title, content, topLeftBuffer) => {
+    return new WinBox({
+        title: title,
+        width: '400px',
+        height: '400px',
+        top: topLeftBuffer,
+        right: 50,
+        bottom: 50,
+        left: topLeftBuffer,
+        mount: content,
+        onfocus: function() {
+            this.setBackground('#00aa00');
+        },
+        onblur: function() {
+            this.setBackground('#777');
+        },
+    })
+};
+
+about.addEventListener('click', () => {
+    createBox('About Me', aboutContent, 50);
+});
+
+contact.addEventListener('click', () => {
+    createBox('Contact Me', contactContent, 150);
+});
